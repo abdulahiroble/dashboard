93ce8c8a71cbd2e9dd42e455eb3dd1031985c884
abdulahiroble <abdulahi_2610@hotmail.com>
2019-12-31 18:54:10 +0100

    display number and proptypes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Widget from "../components/Widget";
+import NumberDisplay from "../components/NumberDisplay";
 import "../styles/Widget.css";
 
 // Import widgets being used in this component
@@ -11,7 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Widget />
+        <Widget heading="Open Ticket Total">
+          <NumberDisplay max={9} value={5} />
+        </Widget>
       </div>
     );
   }
